2b3d8ff5b893d4842c6b080f98b85de6ed6ed8c4
erusek1 <erik@erikrusekelectric.com>
2025-05-17 22:51:24 -0400

    Add MaterialEntryView code-behind

---
+++ ElectricalContractorSystem/Views/MaterialEntryView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace ElectricalContractorSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for MaterialEntryView.xaml
+    /// </summary>
+    public partial class MaterialEntryView : UserControl
+    {
+        public MaterialEntryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
