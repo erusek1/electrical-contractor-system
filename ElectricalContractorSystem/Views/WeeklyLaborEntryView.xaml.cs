98d8566dc0ef1ced8b0f5d4785c320eab40ce9a7
erusek1 <erik@erikrusekelectric.com>
2025-05-17 22:51:12 -0400

    Add WeeklyLaborEntryView code-behind

---
+++ ElectricalContractorSystem/Views/WeeklyLaborEntryView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace ElectricalContractorSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for WeeklyLaborEntryView.xaml
+    /// </summary>
+    public partial class WeeklyLaborEntryView : UserControl
+    {
+        public WeeklyLaborEntryView()
+        {
+            InitializeComponent();
+        }
+    }
+}
