b857113f8b2429cf5edcf8ec846805c3647085a1
erusek1 <erik@erikrusekelectric.com>
2025-05-17 22:50:47 -0400

    Add JobDetailsView code-behind

---
+++ ElectricalContractorSystem/Views/JobDetailsView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace ElectricalContractorSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for JobDetailsView.xaml
+    /// </summary>
+    public partial class JobDetailsView : UserControl
+    {
+        public JobDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
