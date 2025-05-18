4729beca150cf38788e626b8c0a6ff6f9cc3a5c0
erusek1 <erik@erikrusekelectric.com>
2025-05-17 22:42:19 -0400

    Add RelayCommand helper class

---
+++ ElectricalContractorSystem/Helpers/RelayCommand.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Windows.Input;
+
+namespace ElectricalContractorSystem.Helpers
+{
+    /// <summary>
+    /// A command whose sole purpose is to relay its functionality to other objects by invoking delegates.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// Creates a new command that can always execute.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Determines whether this command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data, this object can be set to null.</param>
+        /// <returns>True if this command can be executed; otherwise, false.</returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        /// <summary>
+        /// Executes the command's logic.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data, this object can be set to null.</param>
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
