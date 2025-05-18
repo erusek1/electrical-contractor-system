02a602515aa784c13dc68a5354c98fefa4f1719f
erusek1 <erik@erikrusekelectric.com>
2025-05-17 22:51:44 -0400

    Add JobCostTrackingView code-behind

---
+++ ElectricalContractorSystem/Views/JobCostTrackingView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace ElectricalContractorSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for JobCostTrackingView.xaml
+    /// </summary>
+    public partial class JobCostTrackingView : UserControl
+    {
+        public JobCostTrackingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
