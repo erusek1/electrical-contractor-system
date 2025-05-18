eff6cefae8c2c56c2480a61e524caa75c8f1a394
erusek1 <erik@erikrusekelectric.com>
2025-05-17 22:49:06 -0400

    Add JobManagementView code-behind

---
+++ ElectricalContractorSystem/Views/JobManagementView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows.Controls;
+
+namespace ElectricalContractorSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for JobManagementView.xaml
+    /// </summary>
+    public partial class JobManagementView : UserControl
+    {
+        public JobManagementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
