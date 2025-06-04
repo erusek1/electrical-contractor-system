8a3942a0ffb69061d93c630729098ef5a5ffa2d4
erusek1 <erik@erikrusekelectric.com>
2025-06-04 18:14:48 -0400

    Fix RelayCommand to support both parameterless and generic versions

--- ElectricalContractorSystem/Helpers/RelayCommand.cs
+++ ElectricalContractorSystem/Helpers/RelayCommand.cs
@@ -3,13 +3,16 @@ using System.Windows.Input;
 
 namespace ElectricalContractorSystem.Helpers
 {
-    /// <summary>
-    /// A command whose sole purpose is to relay its functionality to other objects by invoking delegates.
-    /// </summary>
     public class RelayCommand : ICommand
     {
-        private readonly Action<object> _execute;
-        private readonly Predicate<object> _canExecute;
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
 
         public event EventHandler CanExecuteChanged
         {
@@ -17,43 +20,42 @@ namespace ElectricalContractorSystem.Helpers
             remove { CommandManager.RequerySuggested -= value; }
         }
 
-        /// <summary>
-        /// Creates a new command that can always execute.
-        /// </summary>
-        /// <param name="execute">The execution logic.</param>
-        public RelayCommand(Action<object> execute)
-            : this(execute, null)
+        public bool CanExecute(object parameter)
         {
+            return _canExecute == null || _canExecute();
         }
 
-        /// <summary>
-        /// Creates a new command.
-        /// </summary>
-        /// <param name="execute">The execution logic.</param>
-        /// <param name="canExecute">The execution status logic.</param>
-        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
         {
             _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
-        /// <summary>
-        /// Determines whether this command can execute in its current state.
-        /// </summary>
-        /// <param name="parameter">Data used by the command. If the command does not require data, this object can be set to null.</param>
-        /// <returns>True if this command can be executed; otherwise, false.</returns>
-        public bool CanExecute(object parameter)
+        public event EventHandler CanExecuteChanged
         {
-            return _canExecute == null || _canExecute(parameter);
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
         }
 
-        /// <summary>
-        /// Executes the command's logic.
-        /// </summary>
-        /// <param name="parameter">Data used by the command. If the command does not require data, this object can be set to null.</param>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
         public void Execute(object parameter)
         {
-            _execute(parameter);
+            _execute((T)parameter);
         }
     }
 }
