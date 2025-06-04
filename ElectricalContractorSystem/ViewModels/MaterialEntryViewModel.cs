fb83875a628a8022becda15b6900ecf0ecc81419
erusek1 <erik@erikrusekelectric.com>
2025-06-04 19:52:55 -0400

    Fix CS1061 error: Add missing System.Linq using directive

--- ElectricalContractorSystem/ViewModels/MaterialEntryViewModel.cs
+++ ElectricalContractorSystem/ViewModels/MaterialEntryViewModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Windows.Input;
 using ElectricalContractorSystem.Helpers;
 using ElectricalContractorSystem.Models;
