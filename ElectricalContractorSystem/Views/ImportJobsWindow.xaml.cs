61622b55acb762d6c9f0462ae800d62210d5ad67
erusek1 <erik@erikrusekelectric.com>
2025-06-06 17:12:43 -0400

    Create ImportJobsWindow.xaml.cs code-behind

---
+++ ElectricalContractorSystem/Views/ImportJobsWindow.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace ElectricalContractorSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for ImportJobsWindow.xaml
+    /// </summary>
+    public partial class ImportJobsWindow : Window
+    {
+        public ImportJobsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
