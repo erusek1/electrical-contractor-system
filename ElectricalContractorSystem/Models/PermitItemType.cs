cad6d98afaed707396629805cc5643521a6a840c
erusek1 <erik@erikrusekelectric.com>
2025-06-10 15:46:36 -0400

    Remove DataAnnotations from PermitItemType

--- ElectricalContractorSystem/Models/PermitItemType.cs
+++ ElectricalContractorSystem/Models/PermitItemType.cs
@@ -1,23 +1,13 @@
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 
 namespace ElectricalContractorSystem.Models
 {
     public class PermitItemType
     {
         public int PermitTypeId { get; set; }
-        
-        [Required]
-        [StringLength(50)]
         public string Category { get; set; }
-        
-        [Required]
-        [StringLength(255)]
         public string Description { get; set; }
-        
-        [StringLength(20)]
         public string Unit { get; set; } = "each";
-        
         public string Notes { get; set; }
         
         // Navigation properties
