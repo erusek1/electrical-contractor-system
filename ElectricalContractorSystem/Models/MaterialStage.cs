3a3102d3621a53e7fc8479de1dca5035617adcec
erusek1 <erik@erikrusekelectric.com>
2025-06-10 15:46:22 -0400

    Remove DataAnnotations from MaterialStage

--- ElectricalContractorSystem/Models/MaterialStage.cs
+++ ElectricalContractorSystem/Models/MaterialStage.cs
@@ -1,18 +1,10 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace ElectricalContractorSystem.Models
 {
     public class MaterialStage
     {
         public int MaterialStageId { get; set; }
-        
-        [Required]
         public int ItemId { get; set; }
-        
-        [Required]
         public string Stage { get; set; }
-        
-        [Required]
         public decimal MaterialCost { get; set; }
         
         // Navigation property
