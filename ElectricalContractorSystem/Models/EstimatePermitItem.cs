9a2dc707c88c98e169f5df7c71731a3b85c0c955
erusek1 <erik@erikrusekelectric.com>
2025-06-10 15:45:56 -0400

    Remove DataAnnotations from EstimatePermitItem

--- ElectricalContractorSystem/Models/EstimatePermitItem.cs
+++ ElectricalContractorSystem/Models/EstimatePermitItem.cs
@@ -1,20 +1,11 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace ElectricalContractorSystem.Models
 {
     public class EstimatePermitItem
     {
         public int PermitItemId { get; set; }
-        
-        [Required]
         public int EstimateId { get; set; }
-        
-        [Required]
         public int PermitTypeId { get; set; }
-        
-        [Required]
         public int Quantity { get; set; }
-        
         public string Notes { get; set; }
         
         // Navigation properties
