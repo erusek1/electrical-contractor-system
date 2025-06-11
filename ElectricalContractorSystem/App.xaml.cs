6d31ce2c983d0ce37738066d525648b7f101f29d
erusek1 <erik@erikrusekelectric.com>
2025-06-10 20:04:46 -0400

    Add startup diagnostics call to help debug issues

--- ElectricalContractorSystem/App.xaml.cs
+++ ElectricalContractorSystem/App.xaml.cs
@@ -7,5 +7,14 @@ namespace ElectricalContractorSystem
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            
+            // Run diagnostics in debug mode or if requested
+            #if DEBUG
+            StartupTest.RunDiagnostics();
+            #endif
+        }
     }
 }
