1a3ac2ea8a46b2af7b8acacc812d219f1b765d34
erusek1 <erik@erikrusekelectric.com>
2025-06-12 21:56:23 -0400

    Update EstimateLineItem: Add aliases and CreateFromPriceListItem method

--- ElectricalContractorSystem/Models/EstimateLineItem.cs
+++ ElectricalContractorSystem/Models/EstimateLineItem.cs
@@ -6,7 +6,21 @@ namespace ElectricalContractorSystem.Models
         public int RoomId { get; set; }
         public int? ItemId { get; set; }
         public string ItemCode { get; set; }
-        public string Description { get; set; }
+        public string ItemDescription { get; set; }
+        
+        // Aliases for compatibility
+        public string Description 
+        { 
+            get => ItemDescription; 
+            set => ItemDescription = value; 
+        }
+        
+        public string ItemName
+        {
+            get => ItemDescription;
+            set => ItemDescription = value;
+        }
+        
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal MaterialCost { get; set; }
@@ -16,5 +30,20 @@ namespace ElectricalContractorSystem.Models
         
         // Calculated property
         public decimal TotalPrice => Quantity * UnitPrice;
+        
+        // Factory method to create from PriceListItem
+        public static EstimateLineItem CreateFromPriceListItem(PriceListItem item)
+        {
+            return new EstimateLineItem
+            {
+                ItemCode = item.ItemCode,
+                ItemDescription = item.Name,
+                UnitPrice = item.SellPrice,
+                MaterialCost = item.BaseCost,
+                LaborMinutes = item.LaborMinutes,
+                Quantity = 1,
+                LineOrder = 0
+            };
+        }
     }
 }
