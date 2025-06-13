95fae87d0f468d0612141e3c6b0a892d617f9433
erusek1 <erik@erikrusekelectric.com>
2025-06-12 21:56:05 -0400

    Update EstimateRoom: Add LineItems alias and Clone method

--- ElectricalContractorSystem/Models/EstimateRoom.cs
+++ ElectricalContractorSystem/Models/EstimateRoom.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ElectricalContractorSystem.Models
 {
@@ -10,10 +11,45 @@ namespace ElectricalContractorSystem.Models
         public int RoomOrder { get; set; }
         public string Notes { get; set; }
         public List<EstimateLineItem> Items { get; set; }
+        
+        // Alias for compatibility
+        public List<EstimateLineItem> LineItems 
+        { 
+            get => Items; 
+            set => Items = value; 
+        }
 
         public EstimateRoom()
         {
             Items = new List<EstimateLineItem>();
+        }
+        
+        public EstimateRoom Clone()
+        {
+            var clone = new EstimateRoom
+            {
+                RoomName = RoomName,
+                RoomOrder = RoomOrder,
+                Notes = Notes,
+                Items = new List<EstimateLineItem>()
+            };
+            
+            foreach (var item in Items)
+            {
+                clone.Items.Add(new EstimateLineItem
+                {
+                    ItemCode = item.ItemCode,
+                    ItemDescription = item.ItemDescription,
+                    Quantity = item.Quantity,
+                    UnitPrice = item.UnitPrice,
+                    MaterialCost = item.MaterialCost,
+                    LaborMinutes = item.LaborMinutes,
+                    LineOrder = item.LineOrder,
+                    Notes = item.Notes
+                });
+            }
+            
+            return clone;
         }
     }
 }
