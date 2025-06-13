2e945e800bd26d411f93ee3583610aeb71bedf6e
erusek1 <erik@erikrusekelectric.com>
2025-06-13 16:48:04 -0400

    Create code-behind for EmployeeManagementView

---
+++ ElectricalContractorSystem/Views/EmployeeManagementView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace ElectricalContractorSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for EmployeeManagementView.xaml
+    /// </summary>
+    public partial class EmployeeManagementView : Window
+    {
+        public EmployeeManagementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
