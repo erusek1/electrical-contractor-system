eb358b2d591152b59e7773c24e83d6793b1cf4fe
erusek1 <erik@erikrusekelectric.com>
2025-06-13 19:59:49 -0400

    Fixed EstimateRoom to implement INotifyPropertyChanged for room name editing

--- ElectricalContractorSystem/Models/EstimateRoom.cs
+++ ElectricalContractorSystem/Models/EstimateRoom.cs
@@ -1,15 +1,58 @@
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace ElectricalContractorSystem.Models
 {
-    public class EstimateRoom
+    public class EstimateRoom : INotifyPropertyChanged
     {
+        private string _roomName;
+        private int _roomOrder;
+        private string _notes;
+        
         public int RoomId { get; set; }
         public int EstimateId { get; set; }
-        public string RoomName { get; set; }
-        public int RoomOrder { get; set; }
-        public string Notes { get; set; }
+        
+        public string RoomName 
+        { 
+            get => _roomName;
+            set
+            {
+                if (_roomName != value)
+                {
+                    _roomName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        
+        public int RoomOrder 
+        { 
+            get => _roomOrder;
+            set
+            {
+                if (_roomOrder != value)
+                {
+                    _roomOrder = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        
+        public string Notes 
+        { 
+            get => _notes;
+            set
+            {
+                if (_notes != value)
+                {
+                    _notes = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        
         public List<EstimateLineItem> Items { get; set; }
         
         // Alias for compatibility
@@ -18,6 +61,11 @@ namespace ElectricalContractorSystem.Models
             get => Items; 
             set => Items = value; 
         }
+        
+        // Calculated properties
+        public int ItemCount => Items?.Count ?? 0;
+        
+        public decimal RoomTotal => Items?.Sum(i => i.TotalPrice) ?? 0;
 
         public EstimateRoom()
         {
@@ -51,5 +99,16 @@ namespace ElectricalContractorSystem.Models
             
             return clone;
         }
+        
+        #region INotifyPropertyChanged Implementation
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+        #endregion
     }
 }
