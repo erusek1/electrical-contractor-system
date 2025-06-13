62a70170d1e3362b41b3555cfdd959fb25cfd1cc
erusek1 <erik@erikrusekelectric.com>
2025-06-13 16:56:18 -0400

    Create PriceListManagementView code-behind file

---
+++ ElectricalContractorSystem/Views/PriceListManagementView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace ElectricalContractorSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for PriceListManagementView.xaml
+    /// </summary>
+    public partial class PriceListManagementView : Window
+    {
+        public PriceListManagementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
