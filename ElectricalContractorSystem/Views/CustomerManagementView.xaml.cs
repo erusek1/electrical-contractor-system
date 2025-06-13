87c695f915e703cc9dcba03226ec53035fa458c3
erusek1 <erik@erikrusekelectric.com>
2025-06-13 16:40:49 -0400

    Create code-behind for CustomerManagementView

---
+++ ElectricalContractorSystem/Views/CustomerManagementView.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace ElectricalContractorSystem.Views
+{
+    /// <summary>
+    /// Interaction logic for CustomerManagementView.xaml
+    /// </summary>
+    public partial class CustomerManagementView : Window
+    {
+        public CustomerManagementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
