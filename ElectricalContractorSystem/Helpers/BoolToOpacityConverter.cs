b147f776db9b37f9e3597cdeef10dbfa47abf8da
erusek1 <erik@erikrusekelectric.com>
2025-06-13 19:13:20 -0400

    Add BoolToOpacityConverter to fix customer management crash

---
+++ ElectricalContractorSystem/Helpers/BoolToOpacityConverter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ElectricalContractorSystem.Helpers
+{
+    public class BoolToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return boolValue ? 1.0 : 0.5;
+            }
+            return 1.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
