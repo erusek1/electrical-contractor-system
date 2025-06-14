feabc650f089aace1c17617dbd216dbb5916714c
erusek1 <erik@erikrusekelectric.com>
2025-06-14 05:29:15 -0400

    Created MaterialPriceTrackingView code-behind

---
+++ ElectricalContractorSystem/Views/MaterialPriceTrackingView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ElectricalContractorSystem.Views
+{
+    public partial class MaterialPriceTrackingView : UserControl
+    {
+        public MaterialPriceTrackingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
