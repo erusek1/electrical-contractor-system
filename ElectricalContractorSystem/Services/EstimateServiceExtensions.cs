63fef609af088f686a8735a3f7de5d1b977ba74a
erusek1 <erik@erikrusekelectric.com>
2025-06-14 16:00:10 -0400

    Remove duplicate GetEstimateStageSummaries method from EstimateServiceExtensions

--- ElectricalContractorSystem/Services/EstimateServiceExtensions.cs
+++ ElectricalContractorSystem/Services/EstimateServiceExtensions.cs
@@ -12,39 +12,5 @@ namespace ElectricalContractorSystem.Services
         {
             return _databaseService.GetConnection();
         }
-
-        // Add GetEstimateStageSummaries overload with connection parameter
-        public List<EstimateStageSummary> GetEstimateStageSummaries(MySql.Data.MySqlClient.MySqlConnection connection, int estimateId)
-        {
-            var summaries = new List<EstimateStageSummary>();
-            
-            var query = @"
-                SELECT stage, labor_hours, material_cost 
-                FROM EstimateStageSummary 
-                WHERE estimate_id = @estimateId";
-            
-            using (var cmd = new MySql.Data.MySqlClient.MySqlCommand(query, connection))
-            {
-                cmd.Parameters.AddWithValue("@estimateId", estimateId);
-                
-                using (var reader = cmd.ExecuteReader())
-                {
-                    int order = 0;
-                    while (reader.Read())
-                    {
-                        summaries.Add(new EstimateStageSummary
-                        {
-                            EstimateId = estimateId,
-                            Stage = reader.GetString("stage"),
-                            LaborHours = reader.GetDecimal("labor_hours"),
-                            MaterialCost = reader.GetDecimal("material_cost"),
-                            StageOrder = order++
-                        });
-                    }
-                }
-            }
-            
-            return summaries;
-        }
     }
 }
