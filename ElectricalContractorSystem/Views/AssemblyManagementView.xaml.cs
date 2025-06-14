8111563856381a2ba935b89a0f0441aacefc6979
erusek1 <erik@erikrusekelectric.com>
2025-06-13 21:08:38 -0400

    Created AssemblyManagementView code-behind

---
+++ ElectricalContractorSystem/Views/AssemblyManagementView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace ElectricalContractorSystem.Views
+{
+    public partial class AssemblyManagementView : UserControl
+    {
+        public AssemblyManagementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
