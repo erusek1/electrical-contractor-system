a93eb3c38cad217bf03776921042575adb1195e1
erusek1 <erik@erikrusekelectric.com>
2025-06-14 22:03:01 -0400

    Add missing QuickEntryCode_KeyDown event handler

--- ElectricalContractorSystem/Views/EstimateBuilderView.xaml.cs
+++ ElectricalContractorSystem/Views/EstimateBuilderView.xaml.cs
@@ -1,4 +1,6 @@
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ElectricalContractorSystem.Views
 {
@@ -10,6 +12,19 @@ namespace ElectricalContractorSystem.Views
         public EstimateBuilderView()
         {
             InitializeComponent();
+        }
+        
+        private void QuickEntryCode_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                // Try to execute the quick add command when Enter is pressed
+                var viewModel = DataContext as ViewModels.EstimateBuilderViewModel;
+                if (viewModel?.QuickAddCommand?.CanExecute(null) == true)
+                {
+                    viewModel.QuickAddCommand.Execute(null);
+                }
+            }
         }
     }
 }
