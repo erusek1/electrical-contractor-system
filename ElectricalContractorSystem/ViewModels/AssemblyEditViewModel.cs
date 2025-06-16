c1b1601066663bf7ba4128c77df2be7930838f88
erusek1 <erik@erikrusekelectric.com>
2025-06-16 12:08:53 -0400

    Add AssemblyEditViewModel for assembly editing dialogs

--- ElectricalContractorSystem/ViewModels/AssemblyEditViewModel.cs
+++ ElectricalContractorSystem/ViewModels/AssemblyEditViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using ElectricalContractorSystem.Helpers;
@@ -11,49 +12,47 @@ namespace ElectricalContractorSystem.ViewModels
     public class AssemblyEditViewModel : INotifyPropertyChanged
     {
         private AssemblyTemplate _assembly;
-        private ObservableCollection<string> _categories;
-        private bool _isNew;
+        private ObservableCollection<Material> _availableMaterials;
+        private bool _isEditMode;
+        private string _title;
 
         public AssemblyEditViewModel(AssemblyTemplate assembly = null)
         {
-            _isNew = assembly == null;
             _assembly = assembly ?? new AssemblyTemplate
             {
-                AssemblyCode = "",
-                Name = "",
-                Category = "Outlets/Switches",
-                RoughMinutes = 0,
-                FinishMinutes = 0,
-                ServiceMinutes = 0,
-                ExtraMinutes = 0,
-                IsDefault = false,
+                Components = new ObservableCollection<AssemblyComponent>(),
                 IsActive = true,
-                CreatedBy = Environment.UserName,
-                CreatedDate = DateTime.Now
+                IsDefault = false
             };
-
-            LoadCategories();
+            
+            _isEditMode = assembly != null;
+            _title = _isEditMode ? "Edit Assembly" : "Create New Assembly";
+            
+            // Initialize commands
             SaveCommand = new RelayCommand(Save, CanSave);
             CancelCommand = new RelayCommand(Cancel);
+            AddComponentCommand = new RelayCommand(AddComponent);
+            RemoveComponentCommand = new RelayCommand<AssemblyComponent>(RemoveComponent);
         }
 
-        public AssemblyTemplate Assembly
+        public string Title
         {
-            get => _assembly;
+            get => _title;
             set
             {
-                _assembly = value;
+                _title = value;
                 OnPropertyChanged();
             }
         }
 
-        public string AssemblyCode
+        public string Code
         {
-            get => _assembly.AssemblyCode;
+            get => _assembly.Code;
             set
             {
-                _assembly.AssemblyCode = value;
+                _assembly.Code = value;
                 OnPropertyChanged();
+                ((RelayCommand)SaveCommand).RaiseCanExecuteChanged();
             }
         }
 
@@ -64,6 +63,7 @@ namespace ElectricalContractorSystem.ViewModels
             {
                 _assembly.Name = value;
                 OnPropertyChanged();
+                ((RelayCommand)SaveCommand).RaiseCanExecuteChanged();
             }
         }
 
@@ -77,16 +77,6 @@ namespace ElectricalContractorSystem.ViewModels
             }
         }
 
-        public string Category
-        {
-            get => _assembly.Category;
-            set
-            {
-                _assembly.Category = value;
-                OnPropertyChanged();
-            }
-        }
-
         public int RoughMinutes
         {
             get => _assembly.RoughMinutes;
@@ -94,7 +84,6 @@ namespace ElectricalContractorSystem.ViewModels
             {
                 _assembly.RoughMinutes = value;
                 OnPropertyChanged();
-                OnPropertyChanged(nameof(TotalMinutes));
             }
         }
 
@@ -105,7 +94,6 @@ namespace ElectricalContractorSystem.ViewModels
             {
                 _assembly.FinishMinutes = value;
                 OnPropertyChanged();
-                OnPropertyChanged(nameof(TotalMinutes));
             }
         }
 
@@ -116,7 +104,6 @@ namespace ElectricalContractorSystem.ViewModels
             {
                 _assembly.ServiceMinutes = value;
                 OnPropertyChanged();
-                OnPropertyChanged(nameof(TotalMinutes));
             }
         }
 
@@ -127,11 +114,8 @@ namespace ElectricalContractorSystem.ViewModels
             {
                 _assembly.ExtraMinutes = value;
                 OnPropertyChanged();
-                OnPropertyChanged(nameof(TotalMinutes));
             }
         }
-
-        public int TotalMinutes => RoughMinutes + FinishMinutes + ServiceMinutes + ExtraMinutes;
 
         public bool IsDefault
         {
@@ -153,64 +137,82 @@ namespace ElectricalContractorSystem.ViewModels
             }
         }
 
-        public ObservableCollection<string> Categories
+        public ObservableCollection<AssemblyComponent> Components
         {
-            get => _categories;
+            get => _assembly.Components;
             set
             {
-                _categories = value;
+                _assembly.Components = value;
                 OnPropertyChanged();
             }
         }
 
-        public bool IsNew => _isNew;
-        public string Title => IsNew ? "New Assembly" : $"Edit Assembly: {_assembly.Name}";
+        public ObservableCollection<Material> AvailableMaterials
+        {
+            get => _availableMaterials;
+            set
+            {
+                _availableMaterials = value;
+                OnPropertyChanged();
+            }
+        }
 
+        public AssemblyTemplate Assembly => _assembly;
+
+        // Commands
         public ICommand SaveCommand { get; }
         public ICommand CancelCommand { get; }
+        public ICommand AddComponentCommand { get; }
+        public ICommand RemoveComponentCommand { get; }
 
+        // Dialog result
         public bool? DialogResult { get; set; }
-
-        private void LoadCategories()
-        {
-            Categories = new ObservableCollection<string>
-            {
-                "Outlets/Switches",
-                "Lighting",
-                "Panels/Service",
-                "Low Voltage",
-                "HVAC",
-                "Special Systems",
-                "Other"
-            };
-        }
+        
+        // Event to request close
+        public event EventHandler RequestClose;
 
         private bool CanSave()
         {
-            return !string.IsNullOrWhiteSpace(AssemblyCode) &&
-                   !string.IsNullOrWhiteSpace(Name) &&
-                   !string.IsNullOrWhiteSpace(Category);
+            return !string.IsNullOrWhiteSpace(Code) && 
+                   !string.IsNullOrWhiteSpace(Name);
         }
 
         private void Save()
         {
-            if (!_isNew)
-            {
-                _assembly.UpdatedBy = Environment.UserName;
-                _assembly.UpdatedDate = DateTime.Now;
-            }
-
             DialogResult = true;
+            RequestClose?.Invoke(this, EventArgs.Empty);
         }
 
         private void Cancel()
         {
             DialogResult = false;
+            RequestClose?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void AddComponent()
+        {
+            // This would typically show a dialog to select a material
+            // For now, just add a placeholder
+            var component = new AssemblyComponent
+            {
+                Quantity = 1,
+                IsOptional = false
+            };
+            
+            Components.Add(component);
+        }
+
+        private void RemoveComponent(AssemblyComponent component)
+        {
+            if (component != null)
+            {
+                Components.Remove(component);
+            }
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
