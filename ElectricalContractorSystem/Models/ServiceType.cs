ea987b56337f9dc301dc97b93878d9a39c96024a
erusek1 <erik@erikrusekelectric.com>
2025-06-16 11:46:42 -0400

    Add missing properties to ServiceType model

--- ElectricalContractorSystem/Models/ServiceType.cs
+++ ElectricalContractorSystem/Models/ServiceType.cs
@@ -10,15 +10,23 @@ namespace ElectricalContractorSystem.Models
         [StringLength(50)]
         public string Name { get; set; }
         
+        [Required]
+        [StringLength(10)]
+        public string Code { get; set; }
+        
         public string Description { get; set; }
         
         public decimal LaborMultiplier { get; set; } = 1.00m;
+        
+        public decimal BaseHourlyRate { get; set; } = 85.00m;
         
         public decimal MinimumHours { get; set; } = 1.00m;
         
         public decimal DriveTimeIncluded { get; set; } = 0.00m;
         
         public bool IsActive { get; set; } = true;
+        
+        public int SortOrder { get; set; } = 0;
         
         // Calculated properties
         public decimal CalculateRate(decimal baseHourlyRate)
