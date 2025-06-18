86653156db692dffb0a32c2924d0f267d8bc8043
erusek1 <erik@erikrusekelectric.com>
2025-06-18 18:39:24 -0400

    Add missing JobStatus enum for database type conversion

---
+++ ElectricalContractorSystem/Models/JobStatus.cs
@@ -0,0 +1,23 @@
+namespace ElectricalContractorSystem.Models
+{
+    /// <summary>
+    /// Enum representing the status of a job
+    /// </summary>
+    public enum JobStatus
+    {
+        /// <summary>
+        /// Job is in estimate phase
+        /// </summary>
+        Estimate,
+
+        /// <summary>
+        /// Job is in progress
+        /// </summary>
+        InProgress,
+
+        /// <summary>
+        /// Job is complete
+        /// </summary>
+        Complete
+    }
+}
