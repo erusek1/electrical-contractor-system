49a60af90803bfdf76c1d0bd99acc49dbfed0028
erusek1 <erik@erikrusekelectric.com>
2025-06-18 07:25:30 -0400

    Update MaterialPriceTrackingView code-behind to show helpful message if DataContext is missing

--- ElectricalContractorSystem/Views/MaterialPriceTrackingView.xaml.cs
+++ ElectricalContractorSystem/Views/MaterialPriceTrackingView.xaml.cs
@@ -7,6 +7,26 @@ namespace ElectricalContractorSystem.Views
         public MaterialPriceTrackingView()
         {
             InitializeComponent();
+            
+            // Set up test data if no DataContext is provided
+            if (DataContext == null)
+            {
+                this.Loaded += MaterialPriceTrackingView_Loaded;
+            }
+        }
+
+        private void MaterialPriceTrackingView_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            // If DataContext is still null after loading, show a helpful message
+            if (DataContext == null)
+            {
+                System.Windows.MessageBox.Show(
+                    "MaterialPriceTrackingView loaded without DataContext. " +
+                    "This view should be opened through the main menu to work properly.",
+                    "Configuration Issue",
+                    System.Windows.MessageBoxButton.OK,
+                    System.Windows.MessageBoxImage.Information);
+            }
         }
     }
 }
