48bae5d45caea0adf55183c9c3f9b60ffe0d2433
erusek1 <erik@erikrusekelectric.com>
2025-06-18 10:25:13 -0400

    Add CustomerSelectionDialog.xaml.cs

--- ElectricalContractorSystem/Views/CustomerSelectionDialog.xaml.cs
+++ ElectricalContractorSystem/Views/CustomerSelectionDialog.xaml.cs
@@ -1,47 +1,35 @@
 using System.Windows;
-using System.Windows.Input;
-using ElectricalContractorSystem.Models;
 using ElectricalContractorSystem.ViewModels;
+using ElectricalContractorSystem.Services;
+using ElectricalContractorSystem.Models;
 
 namespace ElectricalContractorSystem.Views
 {
     public partial class CustomerSelectionDialog : Window
     {
-        public Customer SelectedCustomer => (DataContext as CustomerSelectionViewModel)?.SelectedCustomer;
-        
-        public CustomerSelectionDialog()
+        public Customer SelectedCustomer { get; private set; }
+        public bool DialogResult { get; private set; }
+
+        public CustomerSelectionDialog(DatabaseService databaseService)
         {
             InitializeComponent();
+            var viewModel = new CustomerSelectionViewModel(databaseService);
+            viewModel.CustomerSelected += OnCustomerSelected;
+            viewModel.DialogCancelled += OnDialogCancelled;
+            DataContext = viewModel;
         }
-        
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
+
+        private void OnCustomerSelected(Customer customer)
+        {
+            SelectedCustomer = customer;
+            DialogResult = true;
+            Close();
+        }
+
+        private void OnDialogCancelled()
         {
             DialogResult = false;
             Close();
-        }
-        
-        private void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            if (SelectedCustomer != null)
-            {
-                DialogResult = true;
-                Close();
-            }
-        }
-        
-        // Handle the select command result
-        protected override void OnSourceInitialized(System.EventArgs e)
-        {
-            base.OnSourceInitialized(e);
-            
-            if (DataContext is CustomerSelectionViewModel viewModel)
-            {
-                viewModel.CloseRequested += (sender, result) =>
-                {
-                    DialogResult = result;
-                    Close();
-                };
-            }
         }
     }
 }
