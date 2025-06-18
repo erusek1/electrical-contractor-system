94921863735ce36e45bf2d8184696fc16e56ba0c
erusek1 <erik@erikrusekelectric.com>
2025-06-18 10:25:32 -0400

    Add CustomerSelectionViewModel.cs

--- ElectricalContractorSystem/ViewModels/CustomerSelectionViewModel.cs
+++ ElectricalContractorSystem/ViewModels/CustomerSelectionViewModel.cs
@@ -1,156 +1,148 @@
 using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
-using System.Windows;
+using System.Runtime.CompilerServices;
 using System.Windows.Input;
-using ElectricalContractorSystem.Helpers;
 using ElectricalContractorSystem.Models;
 using ElectricalContractorSystem.Services;
+using ElectricalContractorSystem.Helpers;
 using ElectricalContractorSystem.Views;
 
 namespace ElectricalContractorSystem.ViewModels
 {
-    public class CustomerSelectionViewModel : ViewModelBase
+    public class CustomerSelectionViewModel : INotifyPropertyChanged
     {
         private readonly DatabaseService _databaseService;
-        private ObservableCollection<Customer> _customers;
+        private ObservableCollection<Customer> _allCustomers;
         private ObservableCollection<Customer> _filteredCustomers;
         private Customer _selectedCustomer;
         private string _searchText;
-        
+
+        public event Action<Customer> CustomerSelected;
+        public event Action DialogCancelled;
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public CustomerSelectionViewModel(DatabaseService databaseService)
         {
             _databaseService = databaseService;
-            
-            Customers = new ObservableCollection<Customer>();
-            FilteredCustomers = new ObservableCollection<Customer>();
-            
-            // Initialize commands
-            SelectCommand = new RelayCommand(ExecuteSelect, CanExecuteSelect);
-            NewCustomerCommand = new RelayCommand(ExecuteNewCustomer);
-            
             LoadCustomers();
+            InitializeCommands();
         }
-        
-        #region Properties
-        
-        public ObservableCollection<Customer> Customers
-        {
-            get => _customers;
-            set => SetProperty(ref _customers, value);
-        }
-        
+
         public ObservableCollection<Customer> FilteredCustomers
         {
             get => _filteredCustomers;
-            set => SetProperty(ref _filteredCustomers, value);
+            set
+            {
+                _filteredCustomers = value;
+                OnPropertyChanged();
+            }
         }
-        
+
         public Customer SelectedCustomer
         {
             get => _selectedCustomer;
             set
             {
-                SetProperty(ref _selectedCustomer, value);
-                CommandManager.InvalidateRequerySuggested();
+                _selectedCustomer = value;
+                OnPropertyChanged();
+                ((RelayCommand)SelectCommand).RaiseCanExecuteChanged();
             }
         }
-        
+
         public string SearchText
         {
             get => _searchText;
             set
             {
-                SetProperty(ref _searchText, value);
+                _searchText = value;
+                OnPropertyChanged();
                 FilterCustomers();
             }
         }
-        
-        #endregion
-        
-        #region Commands
-        
-        public ICommand SelectCommand { get; }
-        public ICommand NewCustomerCommand { get; }
-        
-        #endregion
-        
-        #region Command Implementations
-        
-        private bool CanExecuteSelect(object parameter)
+
+        public ICommand SearchCommand { get; private set; }
+        public ICommand SelectCommand { get; private set; }
+        public ICommand CancelCommand { get; private set; }
+        public ICommand NewCustomerCommand { get; private set; }
+
+        private void InitializeCommands()
         {
-            return SelectedCustomer != null;
+            SearchCommand = new RelayCommand(_ => FilterCustomers());
+            SelectCommand = new RelayCommand(_ => SelectCustomer(), _ => SelectedCustomer != null);
+            CancelCommand = new RelayCommand(_ => Cancel());
+            NewCustomerCommand = new RelayCommand(_ => CreateNewCustomer());
         }
-        
-        private void ExecuteSelect(object parameter)
+
+        private void LoadCustomers()
         {
-            CloseRequested?.Invoke(this, true);
-        }
-        
-        private void ExecuteNewCustomer(object parameter)
-        {
-            var dialog = new AddCustomerDialog();
-            if (dialog.ShowDialog() == true)
+            try
             {
-                var customer = dialog.Customer;
-                _databaseService.SaveCustomer(customer);
+                _allCustomers = new ObservableCollection<Customer>(_databaseService.GetAllCustomers());
+                FilteredCustomers = new ObservableCollection<Customer>(_allCustomers);
+            }
+            catch (Exception ex)
+            {
+                // Log error and show empty collection
+                FilteredCustomers = new ObservableCollection<Customer>();
+            }
+        }
+
+        private void FilterCustomers()
+        {
+            if (string.IsNullOrWhiteSpace(SearchText))
+            {
+                FilteredCustomers = new ObservableCollection<Customer>(_allCustomers);
+            }
+            else
+            {
+                var filtered = _allCustomers.Where(c =>
+                    c.Name.Contains(SearchText, StringComparison.OrdinalIgnoreCase) ||
+                    (!string.IsNullOrEmpty(c.Address) && c.Address.Contains(SearchText, StringComparison.OrdinalIgnoreCase)) ||
+                    (!string.IsNullOrEmpty(c.City) && c.City.Contains(SearchText, StringComparison.OrdinalIgnoreCase)) ||
+                    (!string.IsNullOrEmpty(c.Phone) && c.Phone.Contains(SearchText, StringComparison.OrdinalIgnoreCase))
+                ).ToList();
+
+                FilteredCustomers = new ObservableCollection<Customer>(filtered);
+            }
+        }
+
+        private void SelectCustomer()
+        {
+            if (SelectedCustomer != null)
+            {
+                CustomerSelected?.Invoke(SelectedCustomer);
+            }
+        }
+
+        private void Cancel()
+        {
+            DialogCancelled?.Invoke();
+        }
+
+        private void CreateNewCustomer()
+        {
+            var addCustomerDialog = new AddCustomerDialog(_databaseService);
+            if (addCustomerDialog.ShowDialog() == true && addCustomerDialog.NewCustomer != null)
+            {
+                // Refresh the customer list
                 LoadCustomers();
                 
-                // Select the newly added customer
-                SelectedCustomer = Customers.FirstOrDefault(c => c.CustomerId == customer.CustomerId);
+                // Select the newly created customer
+                SelectedCustomer = _allCustomers.FirstOrDefault(c => c.CustomerId == addCustomerDialog.NewCustomer.CustomerId);
                 
-                // Auto-close with the new customer selected
+                // Auto-select the new customer
                 if (SelectedCustomer != null)
                 {
-                    CloseRequested?.Invoke(this, true);
+                    CustomerSelected?.Invoke(SelectedCustomer);
                 }
             }
         }
-        
-        #endregion
-        
-        #region Private Methods
-        
-        private void LoadCustomers()
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            var customers = _databaseService.GetAllCustomers();
-            
-            Customers.Clear();
-            foreach (var customer in customers.OrderBy(c => c.Name))
-            {
-                Customers.Add(customer);
-            }
-            
-            FilterCustomers();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-        
-        private void FilterCustomers()
-        {
-            var query = Customers.AsEnumerable();
-            
-            if (!string.IsNullOrWhiteSpace(SearchText))
-            {
-                var searchLower = SearchText.ToLower();
-                query = query.Where(c => 
-                    c.Name.ToLower().Contains(searchLower) ||
-                    (c.Address?.ToLower().Contains(searchLower) ?? false) ||
-                    (c.Phone?.ToLower().Contains(searchLower) ?? false) ||
-                    (c.Email?.ToLower().Contains(searchLower) ?? false));
-            }
-            
-            FilteredCustomers.Clear();
-            foreach (var customer in query)
-            {
-                FilteredCustomers.Add(customer);
-            }
-        }
-        
-        #endregion
-        
-        #region Events
-        
-        public event EventHandler<bool> CloseRequested;
-        
-        #endregion
     }
 }
